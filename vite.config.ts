d5b6bea936d99ccb4ecc61b111fa4de0c034ae8f
Romain BG <73529359+rbeget@users.noreply.github.com>
2025-11-26 14:45:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/barcelona-wine-club/',    // 👈 IMPORTANT LINE
+  base: '/barcelona-wine-club/',  
   plugins: [react()],
   build: { outDir: 'build' },
   server: { port: 3000 }
