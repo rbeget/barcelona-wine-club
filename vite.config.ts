3eabd2133e3e271c035afc53dd8a6de7024ca0c4
Romain BG <73529359+rbeget@users.noreply.github.com>
2025-11-26 17:15:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
   export default defineConfig({
     plugins: [react()],
